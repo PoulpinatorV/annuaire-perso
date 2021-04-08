48a3c905dd3a3c9f7ac2da7c885e16ecc42a061c
Corentin Gauci <76254413+PoulpinatorV@users.noreply.github.com>
2021-04-08 15:39:53 +0400

    list games created

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message : 'Votre requête a réussi !' });
-})
+
+
+app.use('/api/games',(req, res, next) => {
+  const games = [
+      {
+        _id : 'odsuhvod',
+        title : 'The last of us',
+        year : 2011,
+        imageUrl : '', 
+      },
+      {
+          _id : 'dsfdsbvu',
+          title : 'Pokemon Blue',
+          year : 1999,
+          imageUrl : '',
+      },
+  ];
+  res.status(200).json(games);
+});
 
 module.exports = app;
